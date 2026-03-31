9e0af26c255f004c8b6f4ea10a6e9f34a1930c5c
Vishal4363 <2203034@ritindia.edu>
2026-03-31 09:54:13 +0530

    Remove .next from tracking

--- apps/web/.next/dev/server/chunks/ssr/[externals]_buffer_058p2v4._.js
+++
@@ -1,8 +0,0 @@
-module.exports = [
-"[externals]/buffer [external] (buffer, cjs)", ((__turbopack_context__, module, exports) => {
-
-const mod = __turbopack_context__.x("buffer", () => require("buffer"));
-
-module.exports = mod;
-}),
-];
